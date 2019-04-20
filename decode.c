4cadff2afe0f0f8f96d62924e7afca28bdf4eb9d
bearbattle <bearbattle@users.noreply.github.com>
2019-04-20 15:54:19 +0800

    更新AC代码

--- decode.c
+++ decode.c
@@ -5,97 +5,91 @@
 
 typedef struct Node
 {
-    char date[20];
-    char Open[1024];
-    char Encrypt[1024];
+    char date[30], O[1100], E[1100];
 } node;
 
-char key[110];
-char key0[110];
-char gg[30];
+char key[110], key0[110], gg[50];
+node message[1100];
 
-node message[1007];
-char Encrypt[1100], Open[1100];
+char f(char src, char k) { return (char)((src + k - 'a' - 'a') % 26 + 'a'); }
+
+char f1(char src, char k) { return (char)((src - k + 26) % 26 + 'a'); }
+
+char g(char src) { return gg[src - 'a']; }
+
 int cmp(const void *a, const void *b)
 {
-    node p, q;
-    p = *(node *)a;
-    q = *(node *)b;
+    node p = *(node *)a;
+    node q = *(node *)b;
     return strcmp(p.date, q.date);
-}
-
-char g(char x) { return gg[(int)x - 'a']; }
-
-char f(char src, char k)
-{
-    char res = (src + tolower(k) - 2 * 'a') % 26 + 'a';
-    //printf("f: %c -> %c\t", src, res);
-    return res;
-}
-
-char f1(char src, char k)
-{
-    char res = (src - k + 52) % 26 + 'a';
-    //printf("f1: %c -> %c\n", src, res);
-    return res;
 }
 
 void ResetKey() { strcpy(key, key0); }
 
 void ChangeKey()
 {
+    int len = (int)strlen(key);
     int i;
-    int len = strlen(key);
-    char t = key[len - 1];
+    char tmp = key[len - 1];
     for (i = len - 1; i > 0; i--)
-    {
         key[i] = key[i - 1];
-    }
-    key[0] = t;
+    key[0] = tmp;
 }
-
+int fucker = 0;
 int main()
 {
-    int i;
     int flag = 0;
     int n;
+    int Cnt = 0;
+    int i;
     int len;
-    int Cnt;
-    scanf("%d\n", &n);
+
+    scanf("%d ", &n);
     gets(key0);
     gets(gg);
-    //printf("get Reflector:%s\n", gg);
+    fprintf(stderr, "Length of key0 is %d, length of gg is %d\n", strlen(key0), strlen(gg));
+
     len = strlen(key0);
+    for (i = 0; key0[i] != '\0'; i++)
+    {
+        key0[i] = tolower(key0[i]);
+    }
+
     for (Cnt = 0; Cnt < n; Cnt++)
     {
-        scanf("%s%s", message[Cnt].date, message[Cnt].Open);
+        memset(message[Cnt].E, 0, sizeof(message[Cnt].E));
+        scanf("%s %s", message[Cnt].date, message[Cnt].O);
+        fprintf(stderr, "Length of date is %d, Length of content is %d\n", (int)strlen(message[Cnt].date),
+                (int)strlen(message[Cnt].O));
         ResetKey();
-        memset(Encrypt, 0, sizeof(Encrypt));
-        memset(Open, 0, sizeof(Open));
-        strcpy(Open, message[Cnt].Open);
-        //printf("DEBUG Message: Key:%s, Origin:%s\n", key, Open);
-        for (i = 0; Open[i] != '\0'; i++)
+
+        for (i = 0; message[Cnt].O[i] != '\0'; i++)
         {
             flag = 0;
+
             if (i > 0 && i % len == 0)
             {
                 ChangeKey();
-                //printf("Key rotated.\n");
             }
-            if (isupper(Open[i]))
-                flag = 1;
-            Encrypt[i] = f1(g(f(tolower(Open[i]), tolower(key[i % len]))), tolower(key[i % len]));
-            if (flag)
+
+            if (isupper(message[Cnt].O[i]))
             {
-                Encrypt[i] = toupper(Encrypt[i]);
+                flag = 1;
+                message[Cnt].O[i] = tolower(message[Cnt].O[i]);
             }
+
+            message[Cnt].E[i] = f1(g(f(message[Cnt].O[i], key[i % len])), key[i % len]);
+            fprintf(stderr, "%d: Encrypting %c using %c, result = %c\n", fucker++, message[Cnt].O[i],
+                    key[i % len], message[Cnt].E[i]);
+            if (flag)
+                message[Cnt].E[i] = toupper(message[Cnt].E[i]);
         }
-        strcpy(message[Cnt].Encrypt, Encrypt);
     }
+
     qsort(message, n, sizeof(message[0]), cmp);
+
     for (Cnt = 0; Cnt < n; Cnt++)
-    {
-        printf("%s %s\n", message[Cnt].date, message[Cnt].Encrypt);
-    }
+        printf("%s %s\n", message[Cnt].date, message[Cnt].E);
+
     return 0;
 }
